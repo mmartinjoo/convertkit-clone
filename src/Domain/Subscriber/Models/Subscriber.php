13845b99b7c76f251c61283e5be1c2a76f80af06
Joó Martin <m4rt1n.j00@gmail.com>
2022-01-26 21:26:21 +0100

    Clean up

--- src/Domain/Subscriber/Models/Subscriber.php
+++ src/Domain/Subscriber/Models/Subscriber.php
@@ -6,7 +6,6 @@ use Domain\Broadcast\Models\Broadcast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Subscriber extends Model
 {
