f7dcda7b5c935b0329148f09ccef288c65076da5
Joó Martin <m4rt1n.j00@gmail.com>
2022-01-26 21:56:08 +0100

    Add import API

--- app/Http/Controllers/Subscriber/ImportSubscribersController.php
+++ app/Http/Controllers/Subscriber/ImportSubscribersController.php
@@ -3,18 +3,13 @@
 namespace App\Http\Controllers\Subscriber;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Domain\Subscriber\Actions\ImportSubscribersAction;
 
 class ImportSubscribersController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        //
+        ImportSubscribersAction::execute(storage_path('subscribers/subscribers.csv'));
+        return response()->noContent();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Subscriber\ImportSubscribersController;
 use App\Http\Controllers\Subscriber\SubscriberController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('subscribers', SubscriberController::class);
+Route::post('subscribers/import', ImportSubscribersController::class);
