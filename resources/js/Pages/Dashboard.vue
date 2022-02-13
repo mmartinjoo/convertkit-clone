c928a8713c1b27b0c665f9043461120fc6045e75
Joó Martin <m4rt1n.j00@gmail.com>
2022-02-13 14:50:43 +0100

    Add performance to dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -46,6 +46,22 @@ export default {
                     <div class="text-xl font-medium text-black">{{ model.new_subscribers_count.total }}</div>
                 </div>
             </div>
+
+            <h2 class="pl-20 mb-2 mt-4">All Time Performance</h2>
+            <div class="sm:px-6 lg:px-8 grid grid-cols-4 gap-2">
+                <div class="p-6 w-52 max-w-sm mx-auto bg-white shadow-md">
+                    <p class="text-gray-500">Average Open Rate</p>
+                    <div class="text-xl font-medium text-black">{{ model.tracking.average_open_rate.formatted }}</div>
+                </div>
+                <div class="p-6 w-52 max-w-sm mx-auto bg-white shadow-md">
+                    <p class="text-gray-500">Average Click Rate</p>
+                    <div class="text-xl font-medium text-black">{{ model.tracking.average_click_rate.formatted }}</div>
+                </div>
+                <div class="p-6 w-52 max-w-sm mx-auto bg-white shadow-md">
+                    <p class="text-gray-500">Total e-mails sent</p>
+                    <div class="text-xl font-medium text-black">{{ model.tracking.total_sent_mails }}</div>
+                </div>
+            </div>
         </div>
 
     </BreezeAuthenticatedLayout>
