3068237ac989b883efa5740bf11bf548429ebacd
Joó Martin <m4rt1n.j00@gmail.com>
2022-02-13 22:12:10 +0100

    Add nav links

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -30,6 +30,9 @@ const showingNavigationDropdown = ref(false);
                                 <BreezeNavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </BreezeNavLink>
+                                <BreezeNavLink :href="route('subscribers.index')" :active="route().current('subscribers.index')">
+                                    Subscribers
+                                </BreezeNavLink>
                             </div>
                         </div>
 
