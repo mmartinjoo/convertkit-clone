d935a6bfa0b1392c179ddf825659d941b15821e1
Joó Martin <m4rt1n.j00@gmail.com>
2022-02-15 19:45:38 +0100

    Use 1 decimal

--- src/Domain/Statistics/ValueObjects/Percent.php
+++ src/Domain/Statistics/ValueObjects/Percent.php
@@ -10,7 +10,7 @@ class Percent
     public function __construct(float $value)
     {
         $this->value = $value;
-        $this->formatted = number_format($this->value * 100, 2) . '%';
+        $this->formatted = number_format($this->value * 100, 1) . '%';
     }
 
     public static function from(float $value): self
