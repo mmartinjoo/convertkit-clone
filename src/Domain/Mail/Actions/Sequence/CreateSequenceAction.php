813d7117c3cc5d4df651c1b6ea74fc78223a3a7d
Joó Martin <m4rt1n.j00@gmail.com>
2022-02-15 21:53:53 +0100

    Create a mail by default

--- src/Domain/Mail/Actions/Sequence/CreateSequenceAction.php
+++ src/Domain/Mail/Actions/Sequence/CreateSequenceAction.php
@@ -3,12 +3,27 @@
 namespace Domain\Mail\Actions\Sequence;
 
 use Domain\Mail\DataTransferObjects\Sequence\SequenceData;
+use Domain\Mail\DataTransferObjects\Sequence\SequenceMailData;
+use Domain\Mail\Enums\Sequence\SequenceMailStatus;
 use Domain\Mail\Models\Sequence\Sequence;
 
 class CreateSequenceAction
 {
     public static function execute(SequenceData $data): Sequence
     {
-        return Sequence::create($data->all());
+        $sequence = Sequence::create($data->all());
+        $mailData = SequenceMailData::from([
+            'subject' => 'My Awesome E-mail',
+            'content' => 'My Awesome Content',
+            'status' => SequenceMailStatus::Draft,
+            'schedule' => [
+                'delay' => 1,
+                'unit' => 'day',
+                'days' => [0,1,2,3,4,5,6],
+            ]
+        ]);
+
+        CreateSequenceMailAction::execute($mailData, $sequence);
+        return $sequence;
     }
 }
