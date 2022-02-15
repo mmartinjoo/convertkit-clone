233fd89cef4249448949e43a966948c080e3d171
Joó Martin <m4rt1n.j00@gmail.com>
2022-02-15 21:54:05 +0100

    Tweak sequence show layout

--- resources/js/Pages/Sequence/Show.vue
+++ resources/js/Pages/Sequence/Show.vue
@@ -19,6 +19,9 @@ export default {
             selectedMail: null,
         };
     },
+    created() {
+        this.selectedMail = this.model.sequence.mails[0];
+    },
     methods: {
         remove() {
             this.$inertia.delete(`/sequences/${this.model.sequence.id}`);
@@ -69,14 +72,14 @@ export default {
             <button @click="remove()" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-3 mt-6" type="button">
                 Delete
             </button>
-            <button v-if="model.sequence.status === 'draft'" @click="publish()" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            <button v-if="model.sequence.status === 'draft'" @click="publish()" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mr-3 rounded focus:outline-none focus:shadow-outline" type="button">
                 Publish
             </button>
             <button @click="addMail()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                 Add E-Mail
             </button>
         </template>
-        <div class="py-12 max-w-7xl mx-auto">
+        <div class="py-12 max-w-7xl mx-auto grid grid-cols-2">
             <form v-if="selectedMail" class="w-full max-w-lg mx-auto" @submit.prevent="submit">
                 <div class="flex flex-wrap -mx-3 mb-6">
                     <div class="w-full px-3 mb-6 md:mb-0">
@@ -96,8 +99,8 @@ export default {
                 </div>
             </form>
 
-            <div class="sm:px-6 lg:px-8 grid grid-cols-1 gap-2">
-                <div v-for="mail in model.sequence.mails" :key="mail.id" @click="selectedMail = mail" class="p-6 w-52 max-w-sm mx-auto bg-white shadow-md hover:cursor-pointer hover:bg-gray-50">
+            <div class="sm:px-6 lg:px-8">
+                <div v-for="mail in model.sequence.mails" :key="mail.id" @click="selectedMail = mail" class="p-6 w-52 max-w-sm mx-auto bg-white shadow-md hover:cursor-pointer hover:bg-gray-50 mb-4">
                     <p class="text-gray-500">{{ mail.subject }}</p>
                 </div>
             </div>
