4c8ed95c4a254013219d23c642215b3f72b95252
Joó Martin <m4rt1n.j00@gmail.com>
2022-02-17 21:21:28 +0100

    Extract out filter show component

---
+++ resources/js/Components/Filter/Show.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: 'FiltersShow',
+    props: {
+        tags: {
+            type: Array,
+            required: true,
+        },
+        forms: {
+            type: Array,
+            required: true,
+        },
+    }
+}
+</script>
+<template>
+    <div>
+        <div>
+            <label class="uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Form filters:
+            </label>
+            <span v-for="form in forms" class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-white bg-gray-400 rounded-full">
+                {{ form.title }}
+            </span>
+        </div>
+        <div>
+            <label class="uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                Tag filters:
+            </label>
+            <span v-for="tag in tags" class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-white bg-blue-400 rounded-full">
+                {{ tag.title }}
+            </span>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Broadcast/Show.vue
+++ resources/js/Pages/Broadcast/Show.vue
@@ -1,9 +1,11 @@
 <script>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
 import { Head, Link } from '@inertiajs/inertia-vue3';
+import FiltersShow from "@/Components/Filter/Show";
 
 export default {
     components: {
+        FiltersShow,
         BreezeAuthenticatedLayout,
         Head,
         Link,
@@ -15,17 +17,17 @@ export default {
         },
     },
     methods: {
-        tagTitle(id) {
-            return this.model.tags.find(tag => tag.id === id).title;
-        },
-        formTitle(id) {
-            return this.model.forms.find(form => form.id === id).title;
-        },
         remove() {
             this.$inertia.delete(`/broadcasts/${this.model.broadcast.id}`);
         },
         send() {
             this.$inertia.patch(`/broadcasts/${this.model.broadcast.id}/send`);
+        },
+        getSelectedTags() {
+            return this.model.tags.filter(tag => this.model.broadcast.filters.form_ids.includes(tag.id));
+        },
+        getSelectedForms() {
+            return this.model.forms.filter(form => this.model.broadcast.filters.form_ids.includes(form.id));
         },
         getPerformance() {
             return `
@@ -57,19 +59,7 @@ export default {
         <div class="py-12 max-w-7xl mx-auto">
             <div v-html="model.broadcast.content" class="mb-6"></div>
 
-            <div v-if="model.broadcast.filters.form_ids.length">
-                Form filters:
-                <span v-for="formId in model.broadcast.filters.form_ids" class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-white bg-gray-400 rounded-full">
-                    {{ formTitle(formId) }}
-                </span>
-            </div>
-            <div v-if="model.broadcast.filters.tag_ids.length">
-                Tag filters:
-                <span v-for="tagId in model.broadcast.filters.tag_ids" class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-white bg-blue-400 rounded-full">
-                    {{ formTitle(tagId) }}
-                </span>
-            </div>
-
+            <FiltersShow :tags="getSelectedTags()" :forms="getSelectedForms()" />
             <button v-if="model.broadcast.status === 'draft'" @click="remove()" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-3 mt-6" type="button">
                 Delete
             </button>
