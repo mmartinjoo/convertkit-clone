761ad6807eb16f68f03f92db851220099d9f384f
Joó Martin <m4rt1n.j00@gmail.com>
2022-02-17 21:10:58 +0100

    Extract out filter form component

--- resources/js/Pages/Broadcast/Form.vue
+++ resources/js/Pages/Broadcast/Form.vue
@@ -1,9 +1,11 @@
 <script>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
 import { Head, Link } from '@inertiajs/inertia-vue3';
+import FiltersForm from "@/Components/Filter/Form";
 
 export default {
     components: {
+        FiltersForm,
         BreezeAuthenticatedLayout,
         Head,
         Link,
@@ -30,6 +32,10 @@ export default {
         submit() {
             this.$inertia.post('/broadcasts', this.form)
         },
+        updateFilters(filters) {
+            this.form.filters.tag_ids = filters.tagIds;
+            this.form.filters.form_ids = filters.formIds;
+        }
     },
 }
 </script>
@@ -61,31 +67,7 @@ export default {
                         <textarea rows="10" v-model="form.content" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="content" type="text" placeholder="HTML content"></textarea>
                     </div>
                 </div>
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                    Filters
-                </label>
-                <div class="flex flex-wrap -mx-3 mb-2">
-                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="form_ids">
-                            Forms
-                        </label>
-                        <div class="relative">
-                            <select v-model="form.filters.form_ids" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="form_ids" multiple>
-                                <option :value="form.id" v-for="form in model.forms" :key="form.id">{{ form.title }}</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="tag_ids">
-                            Tags
-                        </label>
-                        <div class="relative">
-                            <select v-model="form.filters.tag_ids" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="tag_ids" multiple>
-                                <option :value="tag.id" v-for="tag in model.tags" :key="tag.id">{{ tag.title }}</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
+                <FiltersForm :tags="model.tags" :forms="model.forms" @filtersChanged="updateFilters($event)" />
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                     Save
                 </button>
--- src/Domain/Mail/DataTransferObjects/Sequence/SequenceMailData.php
+++ src/Domain/Mail/DataTransferObjects/Sequence/SequenceMailData.php
@@ -26,7 +26,7 @@ class SequenceMailData extends Data
 
     public static function fromRequest(Request $request): self
     {
-        // When creating a new one, the status can be null
+        // When creating a new one, the status and filters can be null
         return self::from([
             ...$request->all(),
             'status' => $request->status ?: SequenceMailStatus::Draft,
