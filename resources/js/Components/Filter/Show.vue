f972a95451f7c94529fb1391ea43fb5906884773
Joó Martin <m4rt1n.j00@gmail.com>
2022-02-17 21:23:37 +0100

    Handle empty filters

--- resources/js/Components/Filter/Show.vue
+++ resources/js/Components/Filter/Show.vue
@@ -19,17 +19,19 @@ export default {
             <label class="uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                 Form filters:
             </label>
-            <span v-for="form in forms" class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-white bg-gray-400 rounded-full">
+            <span v-if="forms.length" v-for="form in forms" :key="form.id" class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-white bg-gray-400 rounded-full">
                 {{ form.title }}
             </span>
+            <span v-else>-</span>
         </div>
         <div>
             <label class="uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                 Tag filters:
             </label>
-            <span v-for="tag in tags" class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-white bg-blue-400 rounded-full">
+            <span v-if="tags.length" v-for="tag in tags" :key="tag.id" class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-white bg-blue-400 rounded-full">
                 {{ tag.title }}
             </span>
+            <span v-else>-</span>
         </div>
     </div>
 </template>
