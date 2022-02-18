f132a48b55a19a1567cf2977c8b5ad0aa2da72c7
Joó Martin <m4rt1n.j00@gmail.com>
2022-02-18 14:46:06 +0100

    Fix tags query

--- src/Domain/Subscriber/Builders/SubscriberBuilder.php
+++ src/Domain/Subscriber/Builders/SubscriberBuilder.php
@@ -8,16 +8,10 @@ use Illuminate\Database\Eloquent\Builder;
 
 class SubscriberBuilder extends Builder
 {
-    /**
-     * Subsriber has exactly the given tag IDs. No more, no less.
-     */
     public function whereTagsExactly(array $ids): self
     {
-        return $this->whereHas('tags', fn (Builder $tags) =>
-            $tags->whereIn('id', $ids)
-        )->whereDoesntHave('tags', fn (Builder $tags) =>
-            $tags->whereNotIn('id', $ids)
-        );
+        return $this->has('tags', '=', count($ids))
+            ->whereHas('tags', fn (Builder $tags) => $tags->whereKey($ids));
     }
 
     public function whereSubscribedBetween(DateFilter $dateFilter): self
