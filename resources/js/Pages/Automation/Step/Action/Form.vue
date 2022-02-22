b560c58bf1fc1b713e0c49bfb71c181b47aeee12
Joó Martin <m4rt1n.j00@gmail.com>
2022-02-22 23:47:10 +0100

    Format form

--- resources/js/Pages/Automation/Step/Action/Form.vue
+++ resources/js/Pages/Automation/Step/Action/Form.vue
@@ -44,11 +44,11 @@ export default {
     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mt-4" for="name">
         Then
     </label>
-    <select name="name" id="name" v-model="action.name">
+    <select name="name" id="name" v-model="action.name" class="appearance-none w-60 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
         <option value="" disabled selected>Select an action</option>
         <option v-for="(name, key) in actions" :key="key" :value="key">{{ name }}</option>
     </select>
-    <select v-if="action.name" name="value" id="value" v-model="action.value" class="ml-2">
+    <select v-if="action.name" name="value" id="value" v-model="action.value" class="ml-2 appearance-none w-60 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
         <option value="" disabled selected>Select a value</option>
         <option v-for="item in values" :key="item.id" :value="item.id">{{ item.title }}</option>
     </select>
--- resources/js/Pages/Automation/Step/Event/Form.vue
+++ resources/js/Pages/Automation/Step/Event/Form.vue
@@ -31,11 +31,11 @@ export default {
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mt-4" for="name">
             When a Subscriber
         </label>
-        <select name="name" id="name" v-model="event.name">
+        <select name="name" id="name" v-model="event.name" class="appearance-none w-60 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
             <option value="" disabled selected>Select an event</option>
             <option v-for="(name, key) in events" :key="key" :value="key">{{ name }}</option>
         </select>
-        <select v-if="event.name" name="value" id="value" v-model="event.value" class="ml-2">
+        <select v-if="event.name" name="value" id="value" v-model="event.value" class="ml-2 appearance-none w-60 bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
             <option value="" disabled selected>Select a value</option>
             <option v-for="item in forms" :key="item.id" :value="item.id">{{ item.title }}</option>
         </select>
