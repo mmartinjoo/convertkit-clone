d00bdd4fe49a6f9e7ddd6eb366ba403410be1717
Joó Martin <m4rt1n.j00@gmail.com>
2022-02-24 17:56:26 +0100

    Add performance line component

---
+++ resources/js/Components/Mail/PerformanceLine.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'PerformanceLine',
+    props: {
+        performance: {
+            type: Object,
+            required: true,
+        },
+        label: {
+            type: String,
+            required: false,
+            default: 'Recipients'
+        }
+    },
+}
+</script>
+<template>
+    <div class="text-sm text-gray-900">
+        {{ performance.total }} {{ label }} • {{ performance.average_open_rate.formatted }} Open rate • {{ performance.average_click_rate.formatted }} Click rate
+    </div>
+</template>
