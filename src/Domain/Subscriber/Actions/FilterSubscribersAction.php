805e98b69c0e1a2e6f40b3cc17d436d581011bf7
Joó Martin <m4rt1n.j00@gmail.com>
2022-03-08 20:11:00 +0100

    Add filters enum

--- src/Domain/Subscriber/Actions/FilterSubscribersAction.php
+++ src/Domain/Subscriber/Actions/FilterSubscribersAction.php
@@ -4,8 +4,8 @@ namespace Domain\Subscriber\Actions;
 
 use Domain\Mail\Contracts\Sendable;
 use Domain\Mail\Models\Sequence\SequenceMail;
-use Domain\Subscriber\Exceptions\InvalidFilterException;
-use Domain\Subscriber\Filters\{Filter, FormFilter, TagFilter};
+use Domain\Subscriber\Enums\Filters;
+use Domain\Subscriber\Filters\Filter;
 use Domain\Subscriber\Models\Subscriber;
 use Illuminate\Pipeline\Pipeline;
 use Illuminate\Support\Collection;
@@ -36,13 +36,7 @@ class FilterSubscribersAction
     {
         return collect($mail->filters()->all())
             ->reject(fn (array $ids) => count($ids) === 0)
-            ->map(fn (array $ids, string $key) =>
-                match ($key) {
-                    'tag_ids' => new TagFilter($ids),
-                    'form_ids' => new FormFilter($ids),
-                    default => InvalidFilterException::because("Filter not found for type {$key}"),
-                }
-            )
+            ->map(fn (array $ids, string $key) => Filters::from($key)->createFilter($ids))
             ->values()
             ->all();
     }
