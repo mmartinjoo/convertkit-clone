882e5dc1f0f33fa73e55d1299c66779494fecadc
Joó Martin <m4rt1n.j00@gmail.com>
2022-03-13 15:33:44 +0100

    Remove sent mails from sendable

--- src/Domain/Mail/Contracts/Sendable.php
+++ src/Domain/Mail/Contracts/Sendable.php
@@ -2,13 +2,10 @@
 
 namespace Domain\Mail\Contracts;
 
-use Illuminate\Database\Eloquent\Relations\Relation;
-
 interface Sendable
 {
     public function id(): int;
     public function subject(): string;
     public function content(): string;
     public function type(): string;
-    public function sent_mails(): Relation;
 }
