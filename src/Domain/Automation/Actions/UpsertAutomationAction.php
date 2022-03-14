22cfe064c23a71398c680847044f019ab50b7e1a
Joó Martin <m4rt1n.j00@gmail.com>
2022-03-14 10:10:09 +0100

    Refactor upsert automation action

--- src/Domain/Automation/Actions/UpsertAutomationAction.php
+++ src/Domain/Automation/Actions/UpsertAutomationAction.php
@@ -14,43 +14,58 @@ class UpsertAutomationAction
     public static function execute(AutomationData $data, User $user): Automation
     {
         return DB::transaction(function () use ($data, $user) {
-            $automation = Automation::updateOrcreate(
-                [
-                    'id' => $data->id,
-                ],
-                [
-                    ...$data->toArray(),
-                    'user_id' => $user->id,
-                ],
-            );
+            $automation = self::upsertAutomation($data, $user);
 
             $automation->steps->each->delete();
 
-            $automation->steps()->updateOrCreate(
-                [
-                    'id' => $data->event->id,
-                ],
-                [
-                    'type' => AutomationStepType::Event,
-                    'name' => $data->event->name,
-                    'value' => ['id' => $data->event->value],
-                ]
-            );
+            self::upsertEvent($automation, $data);
 
-            $data->actions->toCollection()->each(fn (AutomationStepData $stepData) =>
-                $automation->steps()->updateOrcreate(
-                    [
-                        'id' => $stepData->id,
-                    ],
-                    [
-                        'type' => AutomationStepType::Action,
-                        'name' => $stepData->name,
-                        'value' => ['id' => $stepData->value],
-                    ]
-                )
-            );
+            self::upsertActions($automation, $data);
 
             return $automation->load('steps');
         });
+    }
+
+    private static function upsertAutomation(AutomationData $data, User $user): Automation
+    {
+        return Automation::updateOrcreate(
+            [
+                'id' => $data->id,
+            ],
+            [
+                ...$data->toArray(),
+                'user_id' => $user->id,
+            ],
+        );
+    }
+
+    private static function upsertEvent(Automation $automation, AutomationData $data): void
+    {
+        $automation->steps()->updateOrCreate(
+            [
+                'id' => $data->event->id,
+            ],
+            [
+                'type' => AutomationStepType::Event,
+                'name' => $data->event->name,
+                'value' => ['id' => $data->event->value],
+            ]
+        );
+    }
+
+    private static function upsertActions(Automation $automation, AutomationData $data): void
+    {
+        $data->actions->toCollection()->each(fn (AutomationStepData $stepData) =>
+            $automation->steps()->updateOrcreate(
+                [
+                    'id' => $stepData->id,
+                ],
+                [
+                    'type' => AutomationStepType::Action,
+                    'name' => $stepData->name,
+                    'value' => ['id' => $stepData->value],
+                ]
+            )
+        );
     }
 }
